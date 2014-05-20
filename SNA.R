a7d08d8f379b1362407fcdfb46aac53d089a9702
Bastiaan Quast <bquast@gmail.com>
2014-05-20 15:51:07 +0200

    add chapter download instructions

--- SNA.R
+++ SNA.R
@@ -3,3 +3,28 @@
 
 # install the library
 source("http://sna.stanford.edu/setup.R")
+
+# define chapter locations
+ch1 <- 'http://sna.stanford.edu/sna_R_labs/lab_1.R'
+ch2 <- 'http://sna.stanford.edu/sna_R_labs/lab_2.R'
+ch3 <- 'http://sna.stanford.edu/sna_R_labs/lab_3.R'
+ch4 <- 'http://sna.stanford.edu/sna_R_labs/lab_4.R'
+ch5 <- 'http://sna.stanford.edu/sna_R_labs/lab_5.R'
+ch6 <- 'http://sna.stanford.edu/sna_R_labs/lab_6.R'
+ch7 <- 'http://sna.stanford.edu/sna_R_labs/lab_7.R'
+ch8 <- 'http://sna.stanford.edu/sna_R_labs/lab_8.R'
+ch9 <- 'http://sna.stanford.edu/sna_R_labs/lab_9.R'
+ch10 <- 'http://sna.stanford.edu/sna_R_labs/lab_10.R'
+
+
+# download chapters to files
+download.file(ch1, 'ch1.R')
+download.file(ch2, 'ch2.R')
+download.file(ch3, 'ch3.R')
+download.file(ch4, 'ch4.R')
+download.file(ch5, 'ch5.R')
+download.file(ch6, 'ch6.R')
+download.file(ch7, 'ch7.R')
+download.file(ch8, 'ch8.R')
+download.file(ch9, 'ch9.R')
+download.file(ch10, 'ch10.R')
